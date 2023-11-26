11b0e8e7352b7e81012cb0d460078f9f6732f517
Shubham Gulati <shubhamgulati91@gmail.com>
2023-11-26 15:48:40 -0600

    feat(www): initial commit

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -3,7 +3,7 @@ export default {
     // () => "tsc --skipLibCheck --noEmit",
     "eslint --cache --fix --cache-location 'node_modules/.cache/.eslintcache'",
   ],
-  "*.{js,cjs,mjs,ts,tsx,md,mdx,json}": [
+  "*.{js,cjs,mjs,ts,tsx,md,mdx,json,css}": [
     "prettier --write --cache --ignore-path='../../.gitignore'",
   ],
 };
