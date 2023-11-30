605c48b5f2c80d3d578e5ac0bd1dc8a635dfc564
Shubham Gulati <shubhamgulati91@gmail.com>
2023-11-30 10:53:48 -0600

    feat(www): initial commit

--- packages/radix-ui-themes/.eslintrc.js
+++ packages/radix-ui-themes/.eslintrc.js
@@ -1,7 +1,15 @@
-/** @type {import('eslint').Linter.Config} */
-const config = {
+module.exports = {
   root: true,
-  extends: ["@shgulati/eslint-config/base", "@shgulati/eslint-config/react"],
+  extends: ["next", "turbo", "prettier"],
+  rules: {
+    // Not useful for our playground apps:
+    "jsx-a11y/alt-text": "off",
+    "@next/next/no-img-element": "off",
+    "@next/next/no-html-link-for-pages": "off",
+  },
+  parserOptions: {
+    babelOptions: {
+      presets: [require.resolve("next/babel")],
+    },
+  },
 };
-
-module.exports = config;
