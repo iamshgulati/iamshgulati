d34ae3bebc3d1e61487b51b122ca0297a57dd9d3
Shubham Gulati <shubhamgulati91@gmail.com>
2023-11-30 01:03:47 -0600

    feat(www): initial commit

--- apps/www/src/components/command-menu.tsx
+++ apps/www/src/components/command-menu.tsx
@@ -88,222 +88,220 @@ export function CommandMenu({
   }, []);
 
   return (
-    <React.Fragment>
-      <Dialog.Root open={open} onOpenChange={setOpen}>
-        <Dialog.Trigger>
-          <IconButton
-            size="3"
-            variant="ghost"
-            color="gray"
-            onClick={() => setOpen(true)}
-          >
-            <AccessibleIcon label="Search website">
-              <MagnifyingGlassIcon width="20" height="20" />
-            </AccessibleIcon>
-          </IconButton>
-        </Dialog.Trigger>
-        <Dialog.Content className={styles.DialogContent}>
-          <Command loop>
-            <Command.Input placeholder="Type a command or search..." />
+    <Dialog.Root open={open} onOpenChange={setOpen}>
+      <Dialog.Trigger>
+        <IconButton
+          size="3"
+          variant="ghost"
+          color="gray"
+          onClick={() => setOpen(true)}
+        >
+          <AccessibleIcon label="Search website">
+            <MagnifyingGlassIcon width="20" height="20" />
+          </AccessibleIcon>
+        </IconButton>
+      </Dialog.Trigger>
+      <Dialog.Content className={styles.DialogContent}>
+        <Command loop>
+          <Command.Input placeholder="Type a command or search..." />
 
-            <Command.List>
-              <Command.Item
-                value="Theme: Toggle Theme System Light Dark"
-                onSelect={() =>
-                  runCommand(() => {
-                    handleThemeToggle();
-                    // updateThemeClasses();
-                    // updateMetaColor();
-                  })
-                }
-              >
-                <Half2Icon
-                  width="16"
-                  height="16"
-                  style={{
-                    display: "var(--system-theme-icon-display)",
-                    transform: "rotate(45deg)",
-                  }}
-                />
-                <SunIcon
-                  width="16"
-                  height="16"
-                  style={{ display: "var(--light-theme-icon-display)" }}
-                />
-                <MoonIcon
-                  width="16"
-                  height="16"
-                  style={{ display: "var(--dark-theme-icon-display)" }}
-                />
-                Toggle Theme
-                <CommandShortcut>⌘&thinsp;D</CommandShortcut>
-              </Command.Item>
+          <Command.List>
+            <Command.Item
+              value="Theme: Toggle Theme System Light Dark"
+              onSelect={() =>
+                runCommand(() => {
+                  handleThemeToggle();
+                  // updateThemeClasses();
+                  // updateMetaColor();
+                })
+              }
+            >
+              <Half2Icon
+                width="16"
+                height="16"
+                style={{
+                  display: "var(--system-theme-icon-display)",
+                  transform: "rotate(45deg)",
+                }}
+              />
+              <SunIcon
+                width="16"
+                height="16"
+                style={{ display: "var(--light-theme-icon-display)" }}
+              />
+              <MoonIcon
+                width="16"
+                height="16"
+                style={{ display: "var(--dark-theme-icon-display)" }}
+              />
+              Toggle Theme
+              <CommandShortcut>⌘&thinsp;D</CommandShortcut>
+            </Command.Item>
 
-              <Command.Item
-                value="CmdK Command Menu: Toggle CmdK Command Menu"
-                onSelect={() =>
-                  runCommand(() => {
-                    // do nothing
-                  })
-                }
-              >
-                <MagnifyingGlassIcon />
-                Toggle Menu
-                <CommandShortcut>⌘&thinsp;K</CommandShortcut>
-              </Command.Item>
+            <Command.Item
+              value="CmdK Command Menu: Toggle CmdK Command Menu"
+              onSelect={() =>
+                runCommand(() => {
+                  // do nothing
+                })
+              }
+            >
+              <MagnifyingGlassIcon />
+              Toggle Menu
+              <CommandShortcut>⌘&thinsp;K</CommandShortcut>
+            </Command.Item>
 
-              {professionalNav.mainNav.length ? (
-                <Command.Group heading="Professional">
-                  {professionalNav.mainNav.map((item) => {
-                    const ItemIcon: Icon = Icons[item.icon ?? "FileIcon"];
-                    return (
-                      <Command.Item
-                        key={item.href}
-                        value={`Professional Site Pages: ${item.title}`}
-                        data-disabled={item.disabled}
-                        onSelect={() => {
-                          runCommand(() => router.push(item.href as string));
-                        }}
-                      >
-                        <ItemIcon />
-                        {item.title}
-                      </Command.Item>
-                    );
-                  })}
-                </Command.Group>
-              ) : null}
+            {professionalNav.mainNav.length ? (
+              <Command.Group heading="Professional">
+                {professionalNav.mainNav.map((item) => {
+                  const ItemIcon: Icon = Icons[item.icon ?? "FileIcon"];
+                  return (
+                    <Command.Item
+                      key={item.href}
+                      value={`Professional Site Pages: ${item.title}`}
+                      data-disabled={item.disabled}
+                      onSelect={() => {
+                        runCommand(() => router.push(item.href as string));
+                      }}
+                    >
+                      <ItemIcon />
+                      {item.title}
+                    </Command.Item>
+                  );
+                })}
+              </Command.Group>
+            ) : null}
 
-              {personalNav.mainNav.length ? (
-                <Command.Group heading="Personal">
-                  {personalNav.mainNav.map((item) => {
-                    const ItemIcon: Icon = Icons[item.icon ?? "FileIcon"];
-                    return (
-                      <Command.Item
-                        key={item.href}
-                        value={`Personal Site Pages: ${item.title}`}
-                        data-disabled={item.disabled}
-                        onSelect={() => {
-                          runCommand(() => router.push(item.href as string));
-                        }}
-                      >
-                        <ItemIcon />
-                        {item.title}
-                      </Command.Item>
-                    );
-                  })}
-                </Command.Group>
-              ) : null}
+            {personalNav.mainNav.length ? (
+              <Command.Group heading="Personal">
+                {personalNav.mainNav.map((item) => {
+                  const ItemIcon: Icon = Icons[item.icon ?? "FileIcon"];
+                  return (
+                    <Command.Item
+                      key={item.href}
+                      value={`Personal Site Pages: ${item.title}`}
+                      data-disabled={item.disabled}
+                      onSelect={() => {
+                        runCommand(() => router.push(item.href as string));
+                      }}
+                    >
+                      <ItemIcon />
+                      {item.title}
+                    </Command.Item>
+                  );
+                })}
+              </Command.Group>
+            ) : null}
 
-              {socialNav.mainNav.length ? (
-                <Command.Group heading="Social">
-                  {socialNav.mainNav.map((item) => {
-                    const ItemIcon: Icon = Icons[item.icon ?? "AtSymbolIcon"];
-                    return (
-                      <Command.Item
-                        key={item.href}
-                        value={`Social Links: ${item.title}`}
-                        data-disabled={item.disabled}
-                        onSelect={() => {
-                          runCommand(() => router.push(item.href as string));
-                        }}
-                      >
-                        <ItemIcon />
-                        {item.title}
-                        <ArrowTopRightIcon />
-                      </Command.Item>
-                    );
-                  })}
-                </Command.Group>
-              ) : null}
+            {socialNav.mainNav.length ? (
+              <Command.Group heading="Social">
+                {socialNav.mainNav.map((item) => {
+                  const ItemIcon: Icon = Icons[item.icon ?? "AtSymbolIcon"];
+                  return (
+                    <Command.Item
+                      key={item.href}
+                      value={`Social Links: ${item.title}`}
+                      data-disabled={item.disabled}
+                      onSelect={() => {
+                        runCommand(() => router.push(item.href as string));
+                      }}
+                    >
+                      <ItemIcon />
+                      {item.title}
+                      <ArrowTopRightIcon />
+                    </Command.Item>
+                  );
+                })}
+              </Command.Group>
+            ) : null}
 
-              {frontmatter.blogPosts.length ? (
-                <Command.Group heading="Blog Posts">
-                  {frontmatter.blogPosts.map((post) => {
-                    const ItemIcon: Icon = Icons[post.icon ?? "FileTextIcon"];
-                    return (
-                      <Command.Item
-                        key={post.slug}
-                        value={`Blog Posts: ${post.title}`}
-                        onSelect={() => {
-                          runCommand(() => router.push(post.slug));
-                        }}
-                      >
-                        <ItemIcon />
-                        {post.title}
-                      </Command.Item>
-                    );
-                  })}
-                </Command.Group>
-              ) : null}
+            {frontmatter.blogPosts.length ? (
+              <Command.Group heading="Blog Posts">
+                {frontmatter.blogPosts.map((post) => {
+                  const ItemIcon: Icon = Icons[post.icon ?? "FileTextIcon"];
+                  return (
+                    <Command.Item
+                      key={post.slug}
+                      value={`Blog Posts: ${post.title}`}
+                      onSelect={() => {
+                        runCommand(() => router.push(post.slug));
+                      }}
+                    >
+                      <ItemIcon />
+                      {post.title}
+                    </Command.Item>
+                  );
+                })}
+              </Command.Group>
+            ) : null}
 
-              {frontmatter.projects.length ? (
-                <Command.Group heading="Projects">
-                  {frontmatter.projects.map((project) => {
-                    const ItemIcon: Icon = Icons[project.icon ?? "CubeIcon"];
-                    return (
-                      <Command.Item
-                        key={project.slug}
-                        value={`Projects: ${project.title}`}
-                        onSelect={() => {
-                          runCommand(() => router.push(project.slug));
-                        }}
-                      >
-                        <ItemIcon />
-                        {project.title}
-                      </Command.Item>
-                    );
-                  })}
-                </Command.Group>
-              ) : null}
+            {frontmatter.projects.length ? (
+              <Command.Group heading="Projects">
+                {frontmatter.projects.map((project) => {
+                  const ItemIcon: Icon = Icons[project.icon ?? "CubeIcon"];
+                  return (
+                    <Command.Item
+                      key={project.slug}
+                      value={`Projects: ${project.title}`}
+                      onSelect={() => {
+                        runCommand(() => router.push(project.slug));
+                      }}
+                    >
+                      <ItemIcon />
+                      {project.title}
+                    </Command.Item>
+                  );
+                })}
+              </Command.Group>
+            ) : null}
 
-              {frontmatter.thoughts.length ? (
-                <Command.Group heading="Thoughts">
-                  {frontmatter.thoughts.map((thought) => {
-                    const ItemIcon: Icon =
-                      Icons[thought.icon ?? "CrumpledPaperIcon"];
-                    return (
-                      <Command.Item
-                        key={thought.slug}
-                        value={`Thoughts: ${thought.title}`}
-                        onSelect={() => {
-                          runCommand(() => router.push(thought.slug));
-                        }}
-                      >
-                        <ItemIcon />
-                        {thought.title}
-                      </Command.Item>
-                    );
-                  })}
-                </Command.Group>
-              ) : null}
+            {frontmatter.thoughts.length ? (
+              <Command.Group heading="Thoughts">
+                {frontmatter.thoughts.map((thought) => {
+                  const ItemIcon: Icon =
+                    Icons[thought.icon ?? "CrumpledPaperIcon"];
+                  return (
+                    <Command.Item
+                      key={thought.slug}
+                      value={`Thoughts: ${thought.title}`}
+                      onSelect={() => {
+                        runCommand(() => router.push(thought.slug));
+                      }}
+                    >
+                      <ItemIcon />
+                      {thought.title}
+                    </Command.Item>
+                  );
+                })}
+              </Command.Group>
+            ) : null}
 
-              {legalNav.mainNav.length ? (
-                <Command.Group heading="Legal">
-                  {legalNav.mainNav.map((item) => {
-                    const ItemIcon: Icon = Icons[item.icon ?? "FileTextIcon"];
-                    return (
-                      <Command.Item
-                        key={item.href}
-                        value={`Legal Site Pages: ${item.title}`}
-                        data-disabled={item.disabled}
-                        onSelect={() => {
-                          runCommand(() => router.push(item.href as string));
-                        }}
-                      >
-                        <ItemIcon />
-                        {item.title}
-                      </Command.Item>
-                    );
-                  })}
-                </Command.Group>
-              ) : null}
+            {legalNav.mainNav.length ? (
+              <Command.Group heading="Legal">
+                {legalNav.mainNav.map((item) => {
+                  const ItemIcon: Icon = Icons[item.icon ?? "FileTextIcon"];
+                  return (
+                    <Command.Item
+                      key={item.href}
+                      value={`Legal Site Pages: ${item.title}`}
+                      data-disabled={item.disabled}
+                      onSelect={() => {
+                        runCommand(() => router.push(item.href as string));
+                      }}
+                    >
+                      <ItemIcon />
+                      {item.title}
+                    </Command.Item>
+                  );
+                })}
+              </Command.Group>
+            ) : null}
 
-              <Command.Empty>No results found</Command.Empty>
-            </Command.List>
-          </Command>
-        </Dialog.Content>
-      </Dialog.Root>
-    </React.Fragment>
+            <Command.Empty>No results found</Command.Empty>
+          </Command.List>
+        </Command>
+      </Dialog.Content>
+    </Dialog.Root>
   );
 }
 
