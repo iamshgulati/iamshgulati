2ecc03dbb2d48fb8557399b44b742239b2d9076b
Shubham Gulati <shubhamgulati91@gmail.com>
2023-11-30 00:02:53 -0600

    feat(www): initial commit

--- apps/www/next.config.mjs
+++ apps/www/next.config.mjs
@@ -1,7 +1,6 @@
 /** Importing env files here to validate on build */
 import "./src/env.mjs";
 
-import path from "path";
 import bundleAnalyzer from "@next/bundle-analyzer";
 import mdx from "@next/mdx";
 import rehypePrettyCode from "rehype-pretty-code";
@@ -10,45 +9,6 @@ import remarkFrontmatter from "remark-frontmatter";
 import remarkGfm from "remark-gfm";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 
-// eslint-disable-next-line @typescript-eslint/require-await
-const nextRedirects = async () => [
-  {
-    source: "/work",
-    destination: "/work/experience",
-    permanent: false,
-  },
-  {
-    source: "/learn",
-    destination: "/learn/blog",
-    permanent: false,
-  },
-  {
-    source: "/get-resume",
-    destination: "/files/ShubhamGulati_Resume.pdf",
-    permanent: true,
-  },
-  {
-    source: "/linkedin",
-    destination: "https://www.linkedin.com/in/iamshgulati/",
-    permanent: true,
-  },
-  {
-    source: "/twitter",
-    destination: "https://twitter.com/iamshgulati",
-    permanent: true,
-  },
-  {
-    source: "/github",
-    destination: "https://github.com/iamshgulati",
-    permanent: true,
-  },
-  {
-    source: "/mastodon",
-    destination: "https://mastodon.social/@iamshgulati",
-    permanent: true,
-  },
-];
-
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -56,10 +16,9 @@ const nextConfig = {
   /** transpilePackages: ["@shgulati/api", "@shgulati/auth", "@shgulati/db"], */
   transpilePackages: ["@radix-ui/themes"],
   pageExtensions: ["md", "mdx", "ts", "tsx"],
-  redirects: nextRedirects,
   experimental: {
     webpackBuildWorker: true,
-    // mdxRs: true,
+    mdxRs: true,
   },
 
   /** We already do linting and typechecking as separate tasks in CI */
