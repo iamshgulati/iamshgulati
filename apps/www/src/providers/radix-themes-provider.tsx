0924281de7de2a0108f8d036b595b70196133f88
Shubham Gulati <shubhamgulati91@gmail.com>
2023-12-02 22:21:34 -0600

    feat(www): initial commit

--- apps/www/src/providers/radix-themes-provider.tsx
+++ apps/www/src/providers/radix-themes-provider.tsx
@@ -12,7 +12,7 @@ export function RadixThemesProvider({
       scaling="100%"
       radius="medium"
       appearance="inherit"
-      // className="radix-themes-custom-fonts"
+      className="radix-themes-custom-fonts"
     >
       {children}
       {/* {!isProduction && <ThemePanel defaultOpen={false} />} */}
