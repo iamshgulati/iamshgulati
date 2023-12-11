dd876c109bf0c8fd1d84d55b59750038df9dcb47
Shubham Gulati <shubhamgulati91@gmail.com>
2023-12-10 22:28:47 -0600

    feat(www): command-menu — move rules from module css to inline

--- apps/www/src/components/command-menu.tsx
+++ apps/www/src/components/command-menu.tsx
@@ -10,12 +10,11 @@ import {
   MoonIcon,
   SunIcon,
 } from "@radix-ui/react-icons";
-import { AccessibleIcon, Dialog, IconButton, Kbd } from "@radix-ui/themes";
+import { AccessibleIcon, Box, Dialog, IconButton, Kbd } from "@radix-ui/themes";
 import { Command, CommandGroup } from "cmdk";
 import { useTheme } from "next-themes";
 
 import type { AppRoute } from "~/lib/routes";
-import styles from "./command-menu.module.css";
 import type { Icon } from "./icons";
 import { Icons } from "./icons";
 
@@ -111,84 +110,97 @@ export function CommandMenu({
           </AccessibleIcon>
         </IconButton>
       </Dialog.Trigger>
-      <Dialog.Content className={styles.CommandMenuDialogContent}>
-        <Command>
-          <Command.Input placeholder="Type a command or search..." />
+      {/* Inline styles necessary here to override styles defined by RT */}
+      <Box
+        asChild
+        style={{
+          padding: "0",
+          borderRadius: "var(--radius-4)",
+          boxShadow: "none",
+          overflow: "hidden",
+        }}
+      >
+        <Dialog.Content>
+          <Command>
+            <Command.Input placeholder="Type a command or search..." />
 
-          <Command.List>
-            <Command.Empty>No results found</Command.Empty>
+            <Command.List>
+              <Command.Empty>No results found</Command.Empty>
 
-            <CommandGroup heading="Shortcuts">
-              <Command.Item
-                value="Theme: Toggle Theme System Light Dark"
-                onSelect={() => runCommand(() => onThemeToggle())}
-              >
-                <Half2Icon
-                  width="16"
-                  height="16"
-                  style={{
-                    display: "var(--system-theme-icon-display)",
-                    transform: "rotate(45deg)",
-                  }}
-                />
-                <SunIcon
-                  width="16"
-                  height="16"
-                  style={{ display: "var(--light-theme-icon-display)" }}
-                />
-                <MoonIcon
-                  width="16"
-                  height="16"
-                  style={{ display: "var(--dark-theme-icon-display)" }}
-                />
-                Toggle Theme
-                <CommandShortcut>⌘&thinsp;D</CommandShortcut>
-              </Command.Item>
-
-              <Command.Item
-                value="CmdK Command Menu: Toggle CmdK Command Menu"
-                onSelect={() =>
-                  runCommand(() => {
-                    // do nothing
-                  })
-                }
-              >
-                <MagnifyingGlassIcon />
-                Toggle Menu
-                <CommandShortcut>⌘&thinsp;K</CommandShortcut>
-              </Command.Item>
-            </CommandGroup>
-
-            {routes?.map((section, index) =>
-              section.pages.length > 0 ? (
-                <Command.Group
-                  key={section.label ?? index}
-                  heading={section.label}
+              <CommandGroup heading="Shortcuts">
+                <Command.Item
+                  value="Theme: Toggle Theme System Light Dark"
+                  onSelect={() => runCommand(() => onThemeToggle())}
                 >
-                  {section.pages.map((page) => {
-                    const ItemIcon: Icon | undefined =
-                      page.icon && Icons[page.icon];
-                    return (
-                      <Command.Item
-                        key={page.slug}
-                        value={`${section.label}: ${page.title}`}
-                        data-disabled={page.disabled}
-                        onSelect={() => {
-                          runCommand(() => router.push(page.slug));
-                        }}
-                      >
-                        {ItemIcon && <ItemIcon />}
-                        {page.title}
-                        {page.slug.startsWith("http") && <ArrowTopRightIcon />}
-                      </Command.Item>
-                    );
-                  })}
-                </Command.Group>
-              ) : null,
-            )}
-          </Command.List>
-        </Command>
-      </Dialog.Content>
+                  <Half2Icon
+                    width="16"
+                    height="16"
+                    style={{
+                      display: "var(--system-theme-icon-display)",
+                      transform: "rotate(45deg)",
+                    }}
+                  />
+                  <SunIcon
+                    width="16"
+                    height="16"
+                    style={{ display: "var(--light-theme-icon-display)" }}
+                  />
+                  <MoonIcon
+                    width="16"
+                    height="16"
+                    style={{ display: "var(--dark-theme-icon-display)" }}
+                  />
+                  Toggle Theme
+                  <CommandShortcut>⌘&thinsp;D</CommandShortcut>
+                </Command.Item>
+
+                <Command.Item
+                  value="CmdK Command Menu: Toggle CmdK Command Menu"
+                  onSelect={() =>
+                    runCommand(() => {
+                      // do nothing
+                    })
+                  }
+                >
+                  <MagnifyingGlassIcon />
+                  Toggle Menu
+                  <CommandShortcut>⌘&thinsp;K</CommandShortcut>
+                </Command.Item>
+              </CommandGroup>
+
+              {routes?.map((section, index) =>
+                section.pages.length > 0 ? (
+                  <Command.Group
+                    key={section.label ?? index}
+                    heading={section.label}
+                  >
+                    {section.pages.map((page) => {
+                      const ItemIcon: Icon | undefined =
+                        page.icon && Icons[page.icon];
+                      return (
+                        <Command.Item
+                          key={page.slug}
+                          value={`${section.label}: ${page.title}`}
+                          data-disabled={page.disabled}
+                          onSelect={() => {
+                            runCommand(() => router.push(page.slug));
+                          }}
+                        >
+                          {ItemIcon && <ItemIcon />}
+                          {page.title}
+                          {page.slug.startsWith("http") && (
+                            <ArrowTopRightIcon />
+                          )}
+                        </Command.Item>
+                      );
+                    })}
+                  </Command.Group>
+                ) : null,
+              )}
+            </Command.List>
+          </Command>
+        </Dialog.Content>
+      </Box>
     </Dialog.Root>
   );
 }
