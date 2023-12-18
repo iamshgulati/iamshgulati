160e35c43dc8159a91c0013e2a02a24184af703c
Shubham Gulati <shubhamgulati91@gmail.com>
2023-12-18 07:42:57 -0600

    chore(www): classnames — document cn function

--- apps/www/src/lib/classnames.ts
+++ apps/www/src/lib/classnames.ts
@@ -1,2 +1,7 @@
-export const cn = (...classNames: unknown[]) =>
+/**
+ * Concatenates and returns a string of valid class names based on truthy values.
+ * @param {...unknown[]} classNames - An array of values or expressions to evaluate for class names.
+ * @returns {string} - Returns a string of concatenated valid class names.
+ */
+export const cn = (...classNames: unknown[]): string =>
   classNames.filter(Boolean).join(" ");
