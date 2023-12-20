1f38cc332b4ae0e63ecb872cac26062524cbb0c4
Shubham Gulati <shubhamgulati91@gmail.com>
2023-12-20 17:20:45 -0600

    fix(www): revert og — move fonts to public
    This reverts commit 49f875933156cc614c7cf9c9b758054c8831f80f.

--- apps/www/src/app/api/og/route.tsx
+++ apps/www/src/app/api/og/route.tsx
@@ -14,22 +14,19 @@ export const size = {
 
 export async function GET(req: Request) {
   const inter400 = fetch(
-    new URL(
-      "../../../../public/fonts/Inter-4.0/Inter-Regular.woff",
-      import.meta.url,
-    ),
+    new URL("../../../fonts/Inter-4.0/Inter-Regular.woff", import.meta.url),
   ).then((res) => res.arrayBuffer());
 
   const calsans600 = fetch(
     new URL(
-      "../../../../public/fonts/CalSans-1.0.0/CalSans-SemiBold.woff",
+      "../../../fonts/CalSans-1.0.0/CalSans-SemiBold.woff",
       import.meta.url,
     ),
   ).then((res) => res.arrayBuffer());
 
   const playfairLogo700 = fetch(
     new URL(
-      "../../../../public/fonts/Playfair-2.1/Playfair-RegularBold-Logo.woff",
+      "../../../fonts/Playfair-2.1/Playfair-RegularBold-Logo.woff",
       import.meta.url,
     ),
   ).then((res) => res.arrayBuffer());
