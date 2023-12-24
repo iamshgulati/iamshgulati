d14af57b24a23232249575bbca1c5ae0f8145021
Shubham Gulati <shubhamgulati91@gmail.com>
2023-12-24 16:55:35 -0600

    feat(www): about - resume button full width on mobile

--- apps/www/src/data/other/about/resume-button.tsx
+++ apps/www/src/data/other/about/resume-button.tsx
@@ -1,24 +1,30 @@
 import Link from "next/link";
-import { ArrowTopRightIcon, FileTextIcon } from "@radix-ui/react-icons";
+import { ArrowTopRightIcon } from "@radix-ui/react-icons";
 import { Button, Flex, Text } from "@radix-ui/themes";
 
 import { siteConfig } from "~/config/site";
 
 export const ResumeButton = () => (
-  <Flex align="center" justify="center">
+  <Flex
+    direction={{ initial: "column", xs: "row" }}
+    justify="center"
+    style={{
+      textAlign: "center",
+    }}
+  >
     <Link href={siteConfig.links.resume} target="_blank" rel="noreferrer">
       <Button
         variant="solid"
         size="3"
         style={{
+          width: "100%",
           paddingLeft: "var(--space-5)",
           paddingRight: "var(--space-5)",
           gap: "var(--space-2)",
         }}
       >
-        <FileTextIcon width="18" height="18" />
-        <Text>RESUME</Text>
         <ArrowTopRightIcon width="18" height="18" />
+        <Text>RESUME</Text>
       </Button>
     </Link>
   </Flex>
