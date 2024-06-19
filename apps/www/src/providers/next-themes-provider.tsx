7c3a6e0d003dca502fbb5ca8f038f0e5891639f4
Shubham Gulati <shubhamgulati91@gmail.com>
2024-06-19 18:58:06 -0400

    feat(www): theme - switch default from dark to system

--- apps/www/src/providers/next-themes-provider.tsx
+++ apps/www/src/providers/next-themes-provider.tsx
@@ -10,7 +10,7 @@ export const NextThemesProvider = ({
   <ThemeProvider
     {...props}
     attribute="class"
-    defaultTheme="dark"
+    defaultTheme="system"
     enableSystem
     enableColorScheme
     disableTransitionOnChange
