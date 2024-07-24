6d5e685829da15a9cfe20db9b1d5e3f1fb62e69c
Shubham Gulati <shubhamgulati91@gmail.com>
2024-07-23 20:19:00 -0400

    feat(www): change screen size indicator display logic

--- src/components/screen-size-indicator.tsx
+++ src/components/screen-size-indicator.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { Badge, Box, Text } from "@radix-ui/themes";
+import { Badge } from "@radix-ui/themes";
+
+import styles from "./screen-size-indicator.module.css";
 
 export const ScreenSizeIndicator = (): React.JSX.Element => (
   <Badge
@@ -15,80 +17,6 @@ export const ScreenSizeIndicator = (): React.JSX.Element => (
       zIndex: "20",
       pointerEvents: "none",
     }}
-  >
-    <Text size="1">
-      <Box
-        display={{
-          initial: "block",
-          xs: "none",
-          sm: "none",
-          md: "none",
-          lg: "none",
-          xl: "none",
-        }}
-      >
-        i
-      </Box>
-      <Box
-        display={{
-          initial: "none",
-          xs: "block",
-          sm: "none",
-          md: "none",
-          lg: "none",
-          xl: "none",
-        }}
-      >
-        xs
-      </Box>
-      <Box
-        display={{
-          initial: "none",
-          xs: "none",
-          sm: "block",
-          md: "none",
-          lg: "none",
-          xl: "none",
-        }}
-      >
-        sm
-      </Box>
-      <Box
-        display={{
-          initial: "none",
-          xs: "none",
-          sm: "none",
-          md: "block",
-          lg: "none",
-          xl: "none",
-        }}
-      >
-        md
-      </Box>
-      <Box
-        display={{
-          initial: "none",
-          xs: "none",
-          sm: "none",
-          md: "none",
-          lg: "block",
-          xl: "none",
-        }}
-      >
-        lg
-      </Box>
-      <Box
-        display={{
-          initial: "none",
-          xs: "none",
-          sm: "none",
-          md: "none",
-          lg: "none",
-          xl: "block",
-        }}
-      >
-        xl
-      </Box>
-    </Text>
-  </Badge>
+    className={styles.ScreenSizeIndicator}
+  />
 );
