30b16df3ccbf3acf081f5df371b1708ca0f71dda
Shubham Gulati <shubhamgulati91@gmail.com>
2024-08-18 20:42:54 -0400

    fix(www): change site logo in header

--- components/site-logo.tsx
+++ components/site-logo.tsx
@@ -1,29 +1,27 @@
-import { Flex } from "@radix-ui/themes";
+import { Flex, Text } from "@radix-ui/themes";
 import type React from "react";
 
+import { siteConfig } from "~/config/site";
 import type { Icon } from "./icons";
-import { CommandIcon } from "./icons";
+import { HashIcon } from "./icons";
 
 export const SiteLogoIcon = ({
 	...props
 }: React.ComponentPropsWithoutRef<Icon>): React.JSX.Element => {
 	return (
-		<Flex asChild align="center">
-			<CommandIcon width="22" height="22" {...props} />
+		<Flex align="center">
+			<HashIcon width="22" height="22" strokeWidth="0.5" {...props} />
 		</Flex>
 	);
 };
 
-export const SiteLogo = ({
-	style = undefined,
-	...props
-}: React.ComponentProps<typeof SiteLogoIcon>): React.JSX.Element => {
+export const SiteLogo = ({ ...props }: React.ComponentPropsWithoutRef<Icon>): React.JSX.Element => {
 	return (
-		<Flex align="center" gap="2">
-			<SiteLogoIcon style={style} {...props} />
-			{/* <Text size="4" weight="medium" {...props}>
-        {siteConfig.title}
-      </Text> */}
+		<Flex align="center" gap="1">
+			<HashIcon width="22" height="22" strokeWidth="0.5" {...props} />
+			<Text trim="both" style={{ fontSize: "18px", fontWeight: "650", letterSpacing: "0.01em" }}>
+				{siteConfig.title}
+			</Text>
 		</Flex>
 	);
 };
