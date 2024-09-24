4a42da4b3400e0930b89d82e71f7338179d50ed3
iamshgulati <iamshgulati@gmail.com>
2024-09-24 11:54:15 -0400

    feat(www): updated site logo to yinyang

--- components/site-logo.tsx
+++ components/site-logo.tsx
@@ -3,22 +3,22 @@ import type React from "react";
 
 import { siteConfig } from "~/config/site";
 import type { Icon } from "./icons";
-import { HashIcon } from "./icons";
+import { YinYangIcon } from "./icons";
 
 export const SiteLogoIcon = ({
 	...props
 }: React.ComponentPropsWithoutRef<Icon>): React.JSX.Element => {
 	return (
 		<Flex align="center">
-			<HashIcon width="22" height="22" strokeWidth="0.5" {...props} />
+			<YinYangIcon width="22" height="22" {...props} />
 		</Flex>
 	);
 };
 
 export const SiteLogo = ({ ...props }: React.ComponentPropsWithoutRef<Icon>): React.JSX.Element => {
 	return (
-		<Flex align="center" gap="1">
-			<HashIcon width="22" height="22" strokeWidth="0.5" {...props} />
+		<Flex align="center" gap="2">
+			<YinYangIcon width="22" height="22" {...props} />
 			<Text trim="both" style={{ fontSize: "18px", fontWeight: "650", letterSpacing: "0.01em" }}>
 				{siteConfig.title}
 			</Text>
